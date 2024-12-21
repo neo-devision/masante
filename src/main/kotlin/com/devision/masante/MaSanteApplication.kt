b86708638002919fac058e324ecbc1353a811cd2
Naris Razafimahatratra <naris.razafimahatratra@gmail.com>
2024-12-21 20:10:42 +0300

    🎉 Begin project

---
+++ src/main/kotlin/com/devision/masante/MaSanteApplication.kt
@@ -0,0 +1,11 @@
+package com.devision.masante
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MaSanteApplication
+
+fun main(args: Array<String>) {
+	runApplication<MaSanteApplication>(*args)
+}
---
+++ src/test/kotlin/com/devision/masante/MaSanteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devision.masante
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaSanteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
